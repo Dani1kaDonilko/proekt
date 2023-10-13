4fe832f34820f96e08769c66d3c53625a6c876fa
Dani1kaDonilko <daniil.donilko8@gmail.com>
2023-10-13 13:36:56 +0300

    Add files via upload

---
+++ read_sity/read_sity/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace read_sity
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButLog_Click(object sender, RoutedEventArgs e)
+        {
+            UserCabinet userCabinet = new UserCabinet();
+            userCabinet.Show();
+            this.Close();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            RegWindow regWindow = new RegWindow();
+            regWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ read_sity/read_sity/UserCabinet.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace read_sity
+{
+    /// <summary>
+    /// Логика взаимодействия для UserCabinet.xaml
+    /// </summary>
+    public partial class UserCabinet : Window
+    {
+        public UserCabinet()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Home;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Главная";
+        }
+
+        private void Home_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Catalog_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Catalog;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Каталог";
+        }
+
+        private void Catalog_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+       
+
+        private void Profile_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Profile;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Профиль";
+        }
+
+        private void Profile_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+
+        private void Basket_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Basket;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Корзина";
+        }
+
+        private void Basket_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Header_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
